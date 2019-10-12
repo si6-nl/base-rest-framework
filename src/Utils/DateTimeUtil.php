58a279fef260a4146ae38df3e470a628ef76c42f
Reishou <reishou90@gmail.com>
2019-10-12 15:26:09 +0700

    feat(datetime): add getDateString

---
+++ src/Utils/DateTimeUtil.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Si6\Base\Utils;
+
+use Exception;
+use Illuminate\Support\Carbon;
+
+trait DateTimeUtil
+{
+    public function getDateString($value, $oldFormat = 'Ymd')
+    {
+        /** @var Carbon $date */
+        $date = $this->createFromFormat($oldFormat, $value);
+
+        return  $date ? $date->toDateString() : $date;
+    }
+
+    protected function createFromFormat(string $format, string $time)
+    {
+        try {
+            return Carbon::createFromFormat($format, $time);
+        } catch (Exception $exception) {
+            return null;
+        }
+    }
+}

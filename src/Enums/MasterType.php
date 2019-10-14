349dface44b5e745cafb645388326370e755618d
Reishou <reishou90@gmail.com>
2019-10-14 23:06:27 +0700

    feat: add master type enum

---
+++ src/Enums/MasterType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Si6\Base\Enums;
+
+/**
+ * @method static static CYCLING_STADIUM()
+ * @method static static PREFECTURE()
+ * @method static static PROMOTER()
+ * @method static static RANK()
+ * @method static static BICYCLE()
+ * @method static static COUNTRY()
+ * @method static static ARRANGEMENT_CLASSIFICATION()
+ * @method static static PARTICIPATION_CLASSIFICATION()
+ * @method static static REASON_FOR_PARTICIPATION_CHANGE()
+ * @method static static OUTLINE_PROGRAM_SUPPLEMENTATION()
+ * @method static static COMPETITION_TYPE()
+ * @method static static COMPETITION_CLASS()
+ * @method static static ENTRY()
+ * @method static static COMPETITION()
+ * @method static static COMPETITION_DETAIL()
+ * @method static static PERSONAL_CONDITION()
+ * @method static static WINNING_TECHNIQUE()
+ * @method static static DIFFERENCE_IN_ARRIVAL_TIME_DISTANCE()
+ * @method static static NUMBER_OF_RACES_AT_FIRST_TIME()
+ * @method static static EVENT_GRADE()
+ * @method static static OUTLINE_GRADE()
+ * @method static static EVENT_CLASSIFICATION()
+ * @method static static GENDER()
+ * @method static static PERSONAL_CONDITION_SUPPLEMENTATION()
+ */
+class MasterType extends Enum
+{
+    const CYCLING_STADIUM                     = 101;
+    const PREFECTURE                          = 103;
+    const PROMOTER                            = 105;
+    const RANK                                = 109;
+    const BICYCLE                             = 206;
+    const COUNTRY                             = 208;
+    const ARRANGEMENT_CLASSIFICATION          = 301;
+    const PARTICIPATION_CLASSIFICATION        = 302;
+    const REASON_FOR_PARTICIPATION_CHANGE     = 309;
+    const OUTLINE_PROGRAM_SUPPLEMENTATION     = 402;
+    const COMPETITION_TYPE                    = 405;
+    const COMPETITION_CLASS                   = 406;
+    const ENTRY                               = 408;
+    const COMPETITION                         = 411;
+    const COMPETITION_DETAIL                  = 412;
+    const PERSONAL_CONDITION                  = 420;
+    const WINNING_TECHNIQUE                   = 422;
+    const DIFFERENCE_IN_ARRIVAL_TIME_DISTANCE = 423;
+    const NUMBER_OF_RACES_AT_FIRST_TIME       = 425;
+    const EVENT_GRADE                         = 429;
+    const OUTLINE_GRADE                       = 430;
+    const EVENT_CLASSIFICATION                = 452;
+    const GENDER                              = 813;
+    const PERSONAL_CONDITION_SUPPLEMENTATION  = 816;
+}

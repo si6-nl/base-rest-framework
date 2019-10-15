4c09fa5c9f56da5816c0f2504266541e42258b2f
Reishou <reishou90@gmail.com>
2019-10-15 10:02:20 +0700

    fix: add admin status deleted

--- src/Enums/AdminStatus.php
+++ src/Enums/AdminStatus.php
@@ -5,9 +5,11 @@ namespace Si6\Base\Enums;
 /**
  * @method static static ACTIVE()
  * @method static static SUSPENDED()
+ * @method static static DELETED()
  */
 class AdminStatus extends Enum
 {
     const ACTIVE    = 0;
     const SUSPENDED = 1;
+    const DELETED   = 2;
 }

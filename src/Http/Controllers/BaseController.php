9ff56939c9e4eeb9ac6348e7c60dbf542e10adc8
Reishou <reishou90@gmail.com>
2019-10-15 15:03:49 +0700

    feat: add map master with identifier

--- src/Http/Controllers/BaseController.php
+++ src/Http/Controllers/BaseController.php
@@ -6,9 +6,13 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Si6\Base\Enums\MasterType;
 use Si6\Base\Http\Queryable;
 use Si6\Base\Http\ResponseTrait;
+use Throwable;
 
 class BaseController extends Controller
 {
@@ -18,15 +22,25 @@ class BaseController extends Controller
     use ResponseTrait;
     use Queryable;
 
+    /**
+     * @param callable $callback
+     * @return mixed
+     * @throws Throwable
+     */
     protected function transaction(callable $callback)
     {
         return DB::transaction($callback);
     }
 
+    /**
+     * @param $objects
+     * @param string $key
+     * @return Collection
+     */
     protected function mapWithKeys($objects, $key = 'id')
     {
         if (!$objects) {
-            return [];
+            return collect([]);
         }
 
         $objects = collect($objects)->mapWithKeys(function ($object) use ($key) {
@@ -34,5 +48,24 @@ class BaseController extends Controller
         });
 
         return $objects;
+    }
+
+    /**
+     * @param array $masters
+     * @return Collection
+     */
+    protected function mapMasterWithIdentifier(array $masters)
+    {
+        $types = MasterType::toSelectArray();
+
+        $groups = collect($masters)->mapToGroups(function ($master) use ($types) {
+            $key = Str::plural(Str::snake($types[$master->identifier] ?? 'undefined'));
+
+            return [$key => $master];
+        });
+
+        return $groups->map(function ($item) {
+            return $this->mapWithKeys($item, 'code');
+        });
     }
 }

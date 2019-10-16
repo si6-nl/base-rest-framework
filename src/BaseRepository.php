4cffcaad44fb37ad29bbabdba54edfad3b301d08
Reishou <reishou90@gmail.com>
2019-10-16 15:23:20 +0700

    chore: add base repository

---
+++ src/BaseRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Si6\Base;
+
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Builder;
+use Si6\Base\Criteria\Criteria;
+use Si6\Base\Criteria\PaginationOptions;
+use Si6\Base\Criteria\SortOptions;
+
+class BaseRepository
+{
+    /**
+     * @param string $model
+     * @param Criteria|null $criteria
+     * @param SortOptions|null $sort
+     * @return Builder
+     */
+    protected function queryCriteria(string $model, Criteria $criteria = null, SortOptions $sort = null)
+    {
+        $call = $model . '::query';
+
+        /** @var Builder $query */
+        $query = $call();
+
+        if (!$criteria) {
+            $criteria = app(Criteria::class);
+        }
+
+        $criteria->applyQuery($query);
+
+        if (!$sort) {
+            $sort = app(SortOptions::class);
+        }
+
+        $sort->applyQuery($query);
+
+        return $query;
+    }
+
+    /**
+     * @param Builder $query
+     * @param PaginationOptions|null $pagination
+     * @return LengthAwarePaginator
+     */
+    protected function pagination(Builder $query, PaginationOptions $pagination = null)
+    {
+        if (!$pagination) {
+            $pagination = app(PaginationOptions::class);
+        }
+
+        return $pagination->applyQuery($query);
+    }
+}

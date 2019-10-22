f10d2c2e3aa81fec6c048df6f23a48b03e0645cc
Reishou <reishou90@gmail.com>
2019-10-22 16:31:33 +0700

    chore: add vote type enum

---
+++ src/Enums/VoteType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Si6\Base\Enums;
+
+/**
+ * @method static static WIN()
+ * @method static static PLACE_SHOW()
+ * @method static static EXACTA()
+ * @method static static QUINELLA()
+ * @method static static BRACKET_EXACTA()
+ * @method static static BRACKET_QUINELLA()
+ * @method static static TRIFECTA()
+ * @method static static TRIO()
+ * @method static static WIDE()
+ */
+class VoteType extends Enum
+{
+    const WIN              = 10;
+    const PLACE_SHOW       = 20;
+    const EXACTA           = 30;
+    const QUINELLA         = 31;
+    const BRACKET_EXACTA   = 40;
+    const BRACKET_QUINELLA = 41;
+    const TRIFECTA         = 50;
+    const TRIO             = 51;
+    const WIDE             = 60;
+}

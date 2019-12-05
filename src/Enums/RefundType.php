67e1d0bcc77ff2828bb024483087a64b0020ceb6
Reishou <reishou90@gmail.com>
2019-12-05 11:30:01 +0700

    feat: add refund type enum

---
+++ src/Enums/RefundType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Si6\Base\Enums;
+
+/**
+ * @method static static VOTE_WIN()
+ * @method static static SPECIAL()
+ * @method static static RETURN()
+ */
+class RefundType extends Enum
+{
+    const VOTE_WIN = 10;
+    const SPECIAL  = 20;
+    const RETURN   = 30;
+}

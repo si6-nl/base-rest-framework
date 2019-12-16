b07f1c9651d25a77928dd8c1f7118396e4726677
Reishou <reishou90@gmail.com>
2019-12-16 10:44:20 +0700

    fix: merge oddsUtil to BettingUtil

--- src/Utils/BettingUtil.php
+++ src/Utils/BettingUtil.php
@@ -6,6 +6,8 @@ use Si6\Base\Enums\VoteType;
 
 trait BettingUtil
 {
+    protected $_pointers;
+
     protected function getPositionsWins(array $ranks, array $players)
     {
         if (!$ranks || !$players) {
@@ -64,11 +66,138 @@ trait BettingUtil
             }
         }
 
+        $combinations = $this->combinations(array_merge(
+            array_values($positions[0]),
+            array_values($positions[1]),
+            array_values($positions[2])
+        ), 2);
+
+        $positionsWins[VoteType::WIDE] = collect($combinations)->map(function ($combination) use ($sort) {
+            return $sort(array_values($combination));
+        })->toArray();
+
         return $positionsWins;
     }
 
     protected function keyByPosition($item)
     {
         return $item->vote_type . ($item->position_1 ?: '') . ($item->position_2 ?: '') . ($item->position_3 ?: '');
+    }
+
+    public function permutations(array $set, $subset_size = null)
+    {
+        $combinations = $this->combinations($set, $subset_size);
+        $permutations = [];
+        foreach ($combinations as $combination) {
+            $permutations = array_merge($permutations, $this->findPermutations($combination));
+        }
+
+        return $permutations;
+    }
+
+    private function findPermutations($set)
+    {
+        if (count($set) <= 1) {
+            return [$set];
+        }
+        $permutations = [];
+        [$key, $val] = $this->arrayShiftAssoc($set);
+        $sub_permutations = $this->findPermutations($set);
+        foreach ($sub_permutations as $permutation) {
+            $permutations[] = array_merge([$key => $val], $permutation);
+        }
+        $set[$key] = $val;
+        $start_key = $key;
+        $key       = $this->firstKey($set);
+        while ($key != $start_key) {
+            [$key, $val] = $this->arrayShiftAssoc($set);
+            $sub_permutations = $this->findPermutations($set);
+            foreach ($sub_permutations as $permutation) {
+                $permutations[] = array_merge([$key => $val], $permutation);
+            }
+            $set[$key] = $val;
+            $key       = $this->firstKey($set);
+        }
+
+        return $permutations;
+    }
+
+    private function arrayShiftAssoc(array &$array)
+    {
+        foreach ($array as $key => $val) {
+            unset($array[$key]);
+            break;
+        }
+
+        return [$key, $val];
+    }
+
+    private function firstKey($array)
+    {
+        foreach ($array as $key => $val) {
+            break;
+        }
+
+        return $key;
+    }
+
+    public function combinations(array $set, $subset_size = null)
+    {
+        $set_size = count($set);
+        if (is_null($subset_size)) {
+            $subset_size = $set_size;
+        }
+        if ($subset_size >= $set_size) {
+            return [$set];
+        } else {
+            if ($subset_size == 1) {
+                return array_chunk($set, 1);
+            } else {
+                if ($subset_size == 0) {
+                    return [];
+                }
+            }
+        }
+        $combinations    = [];
+        $set_keys        = array_keys($set);
+        $this->_pointers = array_slice(array_keys($set_keys), 0, $subset_size);
+        $combinations[]  = $this->getCombination($set);
+        while ($this->advancePointers($subset_size - 1, $set_size - 1)) {
+            $combinations[] = $this->getCombination($set);
+        }
+
+        return $combinations;
+    }
+
+    private function getCombination($set)
+    {
+        $set_keys    = array_keys($set);
+        $combination = [];
+        foreach ($this->_pointers as $pointer) {
+            $combination[$set_keys[$pointer]] = $set[$set_keys[$pointer]];
+        }
+
+        return $combination;
+    }
+
+    private function advancePointers($pointer_number, $limit)
+    {
+        if ($pointer_number < 0) {
+            return false;
+        }
+        if ($this->_pointers[$pointer_number] < $limit) {
+            $this->_pointers[$pointer_number]++;
+
+            return true;
+        } else {
+            if ($this->advancePointers($pointer_number - 1, $limit - 1)) {
+                $this->_pointers[$pointer_number] =
+                    $this->_pointers[$pointer_number - 1] + 1;
+
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }

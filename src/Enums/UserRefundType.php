ea8e17eb9210165daf5582da1ff1f4e37e2b96b2
Reishou <reishou90@gmail.com>
2019-12-18 11:31:50 +0700

    fix: change enum user refundtype

--- src/Enums/UserRefundType.php
+++ src/Enums/UserRefundType.php
@@ -4,15 +4,15 @@ namespace Si6\Base\Enums;
 
 /**
  * @method static static VOTE_WIN()
- * @method static static RETURN()
+ * @method static static RETURN ()
  * @method static static TRANSFER_TO_BANK()
  * @method static static TRANSFER_TO_DEPOSIT_IN_REFUND_SCREEN()
  * @method static static TRANSFER_TO_DEPOSIT_IN_DEPOSIT_SCREEN()
  */
 class UserRefundType extends Enum
 {
-    const VOTE_WIN                              = 0;
-    const RETURN                                = 1;
+    const VOTE                                  = 0;
+    const EXPIRATION                            = 1;
     const TRANSFER_TO_BANK                      = 2;
     const TRANSFER_TO_DEPOSIT_IN_REFUND_SCREEN  = 3;
     const TRANSFER_TO_DEPOSIT_IN_DEPOSIT_SCREEN = 4;

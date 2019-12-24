d635802709b37085f73fb881c7edaf0d0936141c
Reishou <reishou90@gmail.com>
2019-12-24 11:21:26 +0700

    fix: remove return type in UserRefundType enum

--- src/Enums/UserRefundType.php
+++ src/Enums/UserRefundType.php
@@ -3,8 +3,8 @@
 namespace Si6\Base\Enums;
 
 /**
- * @method static static VOTE_WIN()
- * @method static static RETURN ()
+ * @method static static VOTE()
+ * @method static static EXPIRATION()
  * @method static static TRANSFER_TO_BANK()
  * @method static static TRANSFER_TO_DEPOSIT_IN_REFUND_SCREEN()
  * @method static static TRANSFER_TO_DEPOSIT_IN_DEPOSIT_SCREEN()
@@ -12,9 +12,8 @@ namespace Si6\Base\Enums;
 class UserRefundType extends Enum
 {
     const VOTE                                  = 0;
-    const RETURN                                = 1;
+    const EXPIRATION                            = 1;
     const TRANSFER_TO_BANK                      = 2;
     const TRANSFER_TO_DEPOSIT_IN_REFUND_SCREEN  = 3;
     const TRANSFER_TO_DEPOSIT_IN_DEPOSIT_SCREEN = 4;
-    const EXPIRATION                            = 5;
 }

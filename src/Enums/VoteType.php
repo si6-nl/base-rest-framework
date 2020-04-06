4b965a94df3959672c53508ebde34962cbf4f8b8
Reishou <reishou90@gmail.com>
2020-04-06 19:17:07 +0700

    chore: add getSign to VoteType Enum

--- src/Enums/VoteType.php
+++ src/Enums/VoteType.php
@@ -42,33 +42,77 @@ class VoteType extends Enum
         return self::$text[$value] ?? '';
     }
 
+    public static function getSign($type)
+    {
+        $equals    = [self::QUINELLA, self::BRACKET_QUINELLA, self::TRIO, self::WIDE];
+        $negatives = [self::BRACKET_EXACTA, self::EXACTA, self::TRIFECTA];
+
+        if (in_array($type, $equals)) {
+            return '=';
+        }
+
+        if (in_array($type, $negatives)) {
+            return '-';
+        }
+
+        return '';
+    }
+
+    public static function getOddsSign($item)
+    {
+        $item = (array)$item;
+
+        $sign = self::getSign($item['vote_type']);
+
+        $positions = [$item['position_1']];
+
+        if (!empty($item['position_2'])) {
+            $positions[] = $item['position_2'];
+        }
+
+        if (!empty($item['position_3'])) {
+            $positions[] = $item['position_3'];
+        }
+
+        return implode($sign, $positions);
+    }
+
     public static function isQuinella($type)
     {
-        return in_array($type, [
-            self::QUINELLA,
-            self::BRACKET_QUINELLA,
-            self::TRIO,
-        ]);
+        return in_array(
+            $type,
+            [
+                self::QUINELLA,
+                self::BRACKET_QUINELLA,
+                self::TRIO,
+            ]
+        );
     }
 
     public static function isRequireSecondPosition($type)
     {
-        return in_array($type, [
-            VoteType::EXACTA,
-            VoteType::QUINELLA,
-            VoteType::BRACKET_EXACTA,
-            VoteType::BRACKET_QUINELLA,
-            VoteType::TRIFECTA,
-            VoteType::TRIO,
-            VoteType::WIDE,
-        ]);
+        return in_array(
+            $type,
+            [
+                VoteType::EXACTA,
+                VoteType::QUINELLA,
+                VoteType::BRACKET_EXACTA,
+                VoteType::BRACKET_QUINELLA,
+                VoteType::TRIFECTA,
+                VoteType::TRIO,
+                VoteType::WIDE,
+            ]
+        );
     }
 
     public static function isRequireThirdPosition($type)
     {
-        return in_array($type, [
-            VoteType::TRIFECTA,
-            VoteType::TRIO,
-        ]);
+        return in_array(
+            $type,
+            [
+                VoteType::TRIFECTA,
+                VoteType::TRIO,
+            ]
+        );
     }
 }

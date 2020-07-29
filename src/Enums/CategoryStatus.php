9fe5a367481ad93176a15c2a2ed4cfa7737b078d
Reishou <reishou90@gmail.com>
2020-07-29 14:34:49 +0700

    feat: add enum

---
+++ src/Enums/CategoryStatus.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Si6\Base\Enums;
+
+class CategoryStatus extends Enum
+{
+    public const PUBLIC  = 0;
+    public const PRIVATE = 1;
+}
---
+++ src/Enums/CategoryType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Si6\Base\Enums;
+
+class CategoryType extends Enum
+{
+    public const COLUMN = 0;
+    public const MOVIE  = 1;
+}

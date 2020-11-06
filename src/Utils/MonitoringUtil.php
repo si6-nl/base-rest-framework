b5bfaa8cb18162da6c6f0a901728e0e9c8e0dcba
Reishou <reishou90@gmail.com>
2020-11-07 01:26:01 +0700

    fix: empty body when monitoring

--- src/Utils/MonitoringUtil.php
+++ src/Utils/MonitoringUtil.php
@@ -9,12 +9,7 @@ class MonitoringUtil
     public function services($service)
     {
         Route::get("monitoring/services/$service", function () {
-            $content = json_decode(file_get_contents(base_path('composer.json')));
-
-            $name    = $content->name ?? 'Unknown';
-            $version = $content->version ?? 'Unknown';
-
-            return response()->json(compact('name', 'version'));
+            // just return empty body
         });
     }
 }
